69b699d8d96429e84e5cb024a92d1ee97c1a8396
Krizzuu <47757676+Krizzuu@users.noreply.github.com>
2023-10-01 14:15:01 +0200

    Added strategies for value based models

---
+++ strategy.py
@@ -0,0 +1,87 @@
+from abc import ABC, abstractmethod
+import numpy as np
+import torch
+
+
+class Strategy(ABC):
+    @abstractmethod
+    def select_action(self, model, state):
+        pass
+
+
+class GreedyStrategy(Strategy):
+    def select_action(self, model, state):
+        with torch.no_grad():
+            q_values = model(state).cpu().detach().data.numpy().squeeze()
+        return np.argmax(q_values)
+
+
+class EpsGreedyStrategy(Strategy):
+    def __init__(self, epsilon=0.1):
+        self.epsilon = epsilon
+
+    def select_action(self, model, state):
+        with torch.no_grad():
+            q_values = model(state).cpu().detach().data.numpy().squeeze()
+        if np.random.rand() > self.epsilon:
+            return np.argmax(q_values)
+        else:
+            return np.random.randint(len(q_values))
+
+
+class EpsGreedyLinearStrategy(Strategy):
+    def __init__(self, init_epsilon=1.0, min_epsilon=0.1, decay_steps=10000):
+        self.t = 0
+        self.epsilon = init_epsilon
+        self.init_epsilon = init_epsilon
+        self.min_epsilon = min_epsilon
+        self.decay_steps = decay_steps
+
+    def _update(self):
+        epsilon = 1 - self.t / self.decay_steps
+        epsilon = (self.init_epsilon - self.min_epsilon) * epsilon + self.min_epsilon
+        epsilon = np.clip(epsilon, self.min_epsilon, self.init_epsilon)
+        self.t += 1
+        return epsilon
+
+    def select_action(self, model, state):
+        with torch.no_grad():
+            q_values = model(state).cpu().detach().data.numpy().squeeze()
+        if np.random.rand() > self.epsilon:
+            action = np.argmax(q_values)
+        else:
+            action = np.random.randint(len(q_values))
+
+        self.epsilon = self._update()
+        return action
+
+
+class EpsGreedyExpStrategy(Strategy):
+    def __init__(self, init_epsilon=1.0, min_epsilon=0.1, decay_steps=10000):
+        self.epsilon = init_epsilon
+        self.init_epsilon = init_epsilon
+        self.decay_steps = decay_steps
+        self.min_epsilon = min_epsilon
+        self.epsilons = 0.01 / np.logspace(-2, 0, decay_steps, endpoint=False) - 0.01
+        self.epsilons = self.epsilons * (init_epsilon - min_epsilon) + min_epsilon
+        self.t = 0
+        self.exploratory_action_taken = None
+
+    def _update(self):
+        self.epsilon = self.min_epsilon if self.t >= self.decay_steps else self.epsilons[self.t]
+        self.t += 1
+        return self.epsilon
+
+    def select_action(self, model, state):
+        self.exploratory_action_taken = False
+        with torch.no_grad():
+            q_values = model(state).detach().cpu().data.numpy().squeeze()
+
+        if np.random.rand() > self.epsilon:
+            action = np.argmax(q_values)
+        else:
+            action = np.random.randint(len(q_values))
+
+        self._update()
+        self.exploratory_action_taken = action != np.argmax(q_values)
+        return action
