f1b6477dba6a4f7dd1229e3f92661514beb38467
Krizzuu <kskrzysiek788@gmail.com>
2023-10-04 16:47:13 +0200

    added frame preprocessing, storing experiences and renamed memories in buffer

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import gymnasium as gym
+import cv2 as cv
+
+from model import CnnModel
+from replay_buffer import ReplayBuffer
 
 env_settings = {
     'env_name': 'ALE/Breakout-v5',
@@ -14,14 +12,36 @@ env_settings = {
     'goal_mean_100_reward': 475
 }
 
+
+def process_frame(frame, xy=64):
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    # remove irrelevant parts - scoreboard, lifes
+    frame = frame[31:200, 7:155]
+    frame = cv.resize(frame, (xy, xy), interpolation=cv.INTER_NEAREST)
+    cv.imwrite("./debug_img/test.png", frame)
+    frame = frame.reshape(1, 1, xy, xy)
+    return frame
+
+
+model = CnnModel()
+replay_buffer = ReplayBuffer()
+
 if __name__ == '__main__':
     env = gym.make(env_settings['env_name'], render_mode="human")
     for _ in range(3):  # run for 3 episodes
-        state, _ = env.reset()
+        frame, _ = env.reset()
+        frame = process_frame(frame)
         done = False
         while not done:
             env.render()
-            action = env.action_space.sample()
-            state, reward, done, truncated, info = env.step(action)
+            # action = env.action_space.sample()
+            # action = model.forward(state)  # TODO
+            action = 1
+            new_frame, reward, done, truncated, info = env.step(action)
+            new_frame = process_frame(new_frame)
+            failed = done and not truncated
+            experience = (frame, action, reward, new_frame, float(failed))
+            replay_buffer.store(experience)
+            frame = new_frame
 
 
--- replay_buffer.py
+++ replay_buffer.py
@@ -3,11 +3,11 @@ import numpy as np
 
 class ReplayBuffer:
     def __init__(self, max_size=10000, batch_size=64):
-        self.ss_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # state
-        self.as_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # action
-        self.rs_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # reward
-        self.ps_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # next state
-        self.ds_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # is done
+        self.frame_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # state
+        self.award_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # action
+        self.reward_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # reward
+        self.next_frame_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # next state
+        self.done_mem = np.empty(shape=(max_size), dtype=np.ndarray)  # is done
 
         self.max_size = max_size
         self.batch_size = batch_size
@@ -16,11 +16,11 @@ class ReplayBuffer:
 
     def store(self, sample):
         s, a, r, p, d = sample
-        self.ss_mem[self._idx] = s
-        self.as_mem[self._idx] = a
-        self.rs_mem[self._idx] = r
-        self.ps_mem[self._idx] = p
-        self.ds_mem[self._idx] = d
+        self.frame_mem[self._idx] = s
+        self.award_mem[self._idx] = a
+        self.reward_mem[self._idx] = r
+        self.next_frame_mem[self._idx] = p
+        self.done_mem[self._idx] = d
 
         self._idx += 1
         self._idx = self._idx % self.max_size
@@ -34,11 +34,11 @@ class ReplayBuffer:
 
         idxs = np.random.choice(
             self.size, batch_size, replace=False)
-        experiences = np.vstack(self.ss_mem[idxs]), \
-            np.vstack(self.as_mem[idxs]), \
-            np.vstack(self.rs_mem[idxs]), \
-            np.vstack(self.ps_mem[idxs]), \
-            np.vstack(self.ds_mem[idxs])
+        experiences = np.vstack(self.frame_mem[idxs]), \
+            np.vstack(self.award_mem[idxs]), \
+            np.vstack(self.reward_mem[idxs]), \
+            np.vstack(self.next_frame_mem[idxs]), \
+            np.vstack(self.done_mem[idxs])
         return experiences
 
     def __len__(self):
